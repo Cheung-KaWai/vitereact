583a6dedfa6d736002632de6ff87b39b2ed2e126
Cheung-KaWai <kawaicheungdev@gmail.com>
2022-12-10 23:10:41 +0100

    Add link

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Page1 = () => {
-  return <div>Page1</div>;
+  return (
+    <div>
+      Page1
+      <Link to={"/page2"}>page2</Link>
+    </div>
+  );
 };
--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Page2 = () => {
-  return <div>Page2</div>;
+  return (
+    <div>
+      Page2
+      <Link to={"/page3"}>page3</Link>
+    </div>
+  );
 };
